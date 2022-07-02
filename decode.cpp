92394b547b936e19534c8321027c748023a17a7d
nickname838 <german.galitskov@gmail.com>
2022-07-02 16:35:28 +0700

    add intervals

--- decode.cpp
+++ decode.cpp
@@ -14,7 +14,40 @@ bool comp(para &l, para &r) {
 }
 
 void decode(string file) {
+    ui *alph = new ui[256] {};
+    FILE *in = fopen(file.c_str(), "rb");
+    if(!in) {puts("input doesn't exist"); return;}
 
+    // чтение частот
+    char count, ch;
+    count = fgetc(in);
+    for(int i = 0; i < count; i++) {
+        ch = fgetc(in);
+        if(!feof(in)) fread((char*)&alph[ch], sizeof(alph[ch]), 1, in);
+        else {puts("can't decode2"); return;}
+    }
+
+    // вектор частот
+    vector<para> vec;
+    for(int i = 0; i < 256; i++)
+        if(alph[i]) vec.push_back(make_pair((char)i, alph[i]));
+    sort(vec.begin(), vec.end(), comp);
+
+    for(auto pair: vec) cout << pair.first << ' ' << pair.second << endl;
+
+    // границы
+    unsigned short *inter = new unsigned short[vec.size() + 2];
+    inter[0] = 0;
+    inter[1] = 1;
+    for(int i = 0; i < vec.size(); i++) {
+        ui a = vec[i].second;
+        for(int j = 0; j < i; j++) a += vec[j].second;
+        inter[i + 2] = a;
+    }
+
+    if(inter[vec.size()] > (1 << (codebits - 2) - 1)) {puts("freq error"); return;}
+
+    
 
     ifstream noncod(file, ios::binary);
     ifstream decode(file + ".decoded", ios::binary);
