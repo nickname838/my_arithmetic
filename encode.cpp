b44772895de8d47d12c7f3478278966c2d3e2cd7
nickname838 <german.galitskov@gmail.com>
2022-07-02 16:02:12 +0700

    add a vector of frequencies and boundaries

--- encode.cpp
+++ encode.cpp
@@ -4,9 +4,48 @@
 #include <sys/stat.h>
 
 using namespace std;
+#define codebits 16
+typedef unsigned int ui;
+typedef pair<char, ui> para;
+
+// для сортировки
+bool comp(para &l, para &r) {
+    if(l.second != r.second) return l.second >= r.second;
+    return l.first < r.first;
+}
 
 void encode(string file) {
-    
+    ui *alph = new ui[256] {};
+    FILE *in = fopen(file.c_str(), "rb");
+    if(!in) {puts("input doesn't exist"); return;}
+
+    // алфавит
+    char ch = 0;
+    while(!feof(in)) {
+        ch = fgetc(in);
+        if(!feof(in)) alph[ch]++;
+    }
+    fclose(in);
+
+    // вектор частот
+    vector<para> vec;
+    for(int i = 0; i < 256; i++)
+        if(alph[i]) vec.push_back(make_pair((char)i, alph[i]));
+    sort(vec.begin(), vec.end(), comp);
+
+    for(auto pair: vec) cout << pair.first << " " << pair.second << endl;
+
+    // границы
+    unsigned short *inter = new unsigned short[vec.size() + 2];
+    inter[0] = 0;
+    inter[1] = 1;
+    for(int i = 0; i < vec.size(); i++) {
+        ui a = vec[i].second;
+        for(int j = 0; j < i; j++) a += vec[j].second;
+        inter[i + 2] = a;
+    }
+
+    if(inter[vec.size()] > (1 << (codebits - 2) - 1)) {puts("freq error"); return;}
 
     // сравнение размеров
     struct stat sb; int bsize = 0;
